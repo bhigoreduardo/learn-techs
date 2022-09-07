6d162c699db82e02bdd460b2179994ef259ae060
bhigoreduardo <bhigoreduardo@gmail.com>
2022-09-07 10:37:37 -0300

    Created backend Spring Boot

---
+++ dsmeta-devsuperior/backend/src/test/java/com/dashboard/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
