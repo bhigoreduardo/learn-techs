a463204d88b79c76bebce3992d312e1279be91a0
bhigoreduardo <bhigoreduardo@gmail.com>
2022-09-07 14:21:46 -0300

    Create Get Param Twilio SendSms

--- dsmeta-devsuperior/backend/src/main/java/com/dashboard/dsmeta/controllers/SaleController.java
+++ dsmeta-devsuperior/backend/src/main/java/com/dashboard/dsmeta/controllers/SaleController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,9 +31,9 @@ public class SaleController {
 		return saleService.selectAllSales(minDate, maxDate, pageable);
 	}
 	
-	@GetMapping("/sms")
-	public void sendSms() {
-		smsService.sendSms();
+	@GetMapping("/sms/{id}")
+	public void sendSms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 
 }
--- dsmeta-devsuperior/backend/src/main/java/com/dashboard/dsmeta/services/SmsService.java
+++ dsmeta-devsuperior/backend/src/main/java/com/dashboard/dsmeta/services/SmsService.java
@@ -1,8 +1,11 @@
 package com.dashboard.dsmeta.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.dashboard.dsmeta.entities.Sale;
+import com.dashboard.dsmeta.repositories.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -22,9 +25,17 @@ public class SmsService {
 	@Value(value = "${twilio.phone.to}")
 	private String twilioPhoneTo;
 
-	public void sendSms() {
+	@Autowired
+	private SaleRepository saleRepository;
 
-		String msg = "teste";
+	public void sendSms(Long id) {
+
+		Sale sale = saleRepository.findById(id).get();
+
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+
+		String msg = "O vendedor: " + sale.getSellerName() + " no período: " + date + " teve o faturamento de R$ "
+				+ String.format("%.2f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
