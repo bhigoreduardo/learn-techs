055a4036707ac6231c399f82e017649cc606560a
bhigoreduardo <bhigoreduardo@gmail.com>
2022-09-10 14:54:29 -0300

    Created Button Props

--- dsmeta-devsuperior/frontend/src/components/Button/index.tsx
+++ dsmeta-devsuperior/frontend/src/components/Button/index.tsx
@@ -1,12 +1,23 @@
+import axios from "axios";
+
 import icon from "../../assets/icons/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 
 import "./styles.css";
 
-function Button() {
+type Props = {
+  saleId: number;
+};
+
+function sendNotification(saleId: number) {
+  axios.get(`${BASE_URL}/sales/sms/${saleId}`).then((response) => console.log("Mensagem enviada com sucesso"));
+}
+
+function Button({ saleId }: Props) {
   return (
     <>
       <div className="dsmeta-red-btn">
-        <img src={icon} alt="Notificar" />
+        <img src={icon} alt="Notificar" onClick={() => sendNotification(saleId)} />
       </div>
     </>
   );
--- dsmeta-devsuperior/frontend/src/components/Table/index.tsx
+++ dsmeta-devsuperior/frontend/src/components/Table/index.tsx
@@ -76,7 +76,7 @@ function Table() {
                     <td>R$ {sale.amount.toFixed(2)}</td>
                     <td>
                       <div className="dsmeta-red-btn-container">
-                        <Button />
+                        <Button saleId={sale.id} />
                       </div>
                     </td>
                   </tr>
